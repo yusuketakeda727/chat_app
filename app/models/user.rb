c2ae5e602ac194d2c6c4ac850c9c514c1b6d752c
yusuketakeda727 <77325226+yusuketakeda727@users.noreply.github.com>
2021-01-23 13:02:56 +0900

    バリデーションの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,7 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true   # nameが空の場合は、DBに保存しないというバリデーションを設定する。
+
 end
