89c19ee79e2400654f877013b3a182ea0de58e76
yusuketakeda727 <77325226+yusuketakeda727@users.noreply.github.com>
2021-01-23 20:58:11 +0900

    チャットルーム作成機能の実装

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,21 @@
 class RoomsController < ApplicationController
+
   def new   # ルームコントローラーの実装
     @room = Room.new
+  end
+
+  def create    # 下記の記述でDBに値を保存できるようにする。
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:name, user_ids: [])
   end
 end
